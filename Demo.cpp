5e6e81de4db31f917e798cd923459bee6a4ae688
MasgataNK <masgata25@student.ub.ac.id>
2021-12-05 20:44:47 +0700

    Tugas 4

--- Demo.cpp
+++ Demo.cpp
@@ -15,33 +15,31 @@ Demo::~Demo() {
 void Demo::Init() {
 	// build and compile our shader program
 	// ------------------------------------
-	shaderProgram = BuildShader("vertexShader.vert", "fragmentShader.frag", nullptr);
-
-	BuildColoredLemari();
-
-	BuildColoredLantai();
-
-	InitCamera();
+	BuildShaders();
+	BuildDepthMap();
+	BuildTexturedCube();
+	BuildTexturedPlane();
 }
 
 void Demo::DeInit() {
 	// optional: de-allocate all resources once they've outlived their purpose:
 	// ------------------------------------------------------------------------
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
-	glDeleteVertexArrays(1, &VAO2);
-	glDeleteBuffers(1, &VBO2);
-	glDeleteBuffers(1, &EBO2);
+	glDeleteVertexArrays(1, &cubeVAO);
+	glDeleteBuffers(1, &cubeVBO);
+	glDeleteBuffers(1, &cubeEBO);
+	glDeleteVertexArrays(1, &planeVAO);
+	glDeleteBuffers(1, &planeVBO);
+	glDeleteBuffers(1, &planeEBO);
+	glDeleteBuffers(1, &depthMapFBO);
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
-void Demo::ProcessInput(GLFWwindow *window) {
+void Demo::ProcessInput(GLFWwindow* window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, true);
 	}
-	
+
 	// zoom camera
 	// -----------
 	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS) {
@@ -76,8 +74,8 @@ void Demo::ProcessInput(GLFWwindow *window) {
 	// update camera rotation
 	// ----------------------
 	double mouseX, mouseY;
-	double midX = screenWidth/2;
-	double midY = screenHeight/2;
+	double midX = screenWidth / 2;
+	double midY = screenHeight / 2;
 	float angleY = 0.0f;
 	float angleZ = 0.0f;
 
@@ -107,7 +105,7 @@ void Demo::ProcessInput(GLFWwindow *window) {
 	RotateCamera(-angleY);
 
 
-	
+
 
 }
 
@@ -116,87 +114,137 @@ void Demo::Update(double deltaTime) {
 }
 
 void Demo::Render() {
+	glEnable(GL_DEPTH_TEST);
+
 	glViewport(0, 0, this->screenWidth, this->screenHeight);
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
-	glEnable(GL_DEPTH_TEST);
+	// Step 1 Render depth of scene to texture
+   // ----------------------------------------
+	glm::mat4 lightProjection, lightView;
+	glm::mat4 lightSpaceMatrix;
+	float near_plane = 1.0f, far_plane = 7.5f;
+	lightProjection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, near_plane, far_plane);
+	lightView = glm::lookAt(glm::vec3(-2.0f, 4.0f, -1.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0, 1.0, 0.0)); // -2.0f, 4.0f, -1.0f
+	lightSpaceMatrix = lightProjection * lightView;
+
+	// render scene from light's point of view
+	UseShader(this->depthmapShader);
+	glUniformMatrix4fv(glGetUniformLocation(this->depthmapShader, "lightSpaceMatrix"), 1, GL_FALSE, glm::value_ptr(lightSpaceMatrix));
+	glViewport(0, 0, this->SHADOW_WIDTH, this->SHADOW_HEIGHT);
+	glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+	glClear(GL_DEPTH_BUFFER_BIT);
+	DrawTexturedCube(this->depthmapShader);
+	DrawTexturedPlane(this->depthmapShader);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	
+	// Step 2 Render scene normally using generated depth map
+	// ------------------------------------------------------
+	glViewport(0, 0, this->screenWidth, this->screenHeight);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	// Pass perspective projection matrix
-	glm::mat4 projection = glm::perspective(fovy, (GLfloat)this->screenWidth / (GLfloat)this->screenHeight, 0.1f, 100.0f);
-	GLint projLoc = glGetUniformLocation(this->shaderProgram, "projection");
-	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
-
+	UseShader(this->shadowmapShader);
+	glm::mat4 projection = glm::perspective(45.0f, (GLfloat)this->screenWidth / (GLfloat)this->screenHeight, 0.1f, 100.0f);
+	glUniformMatrix4fv(glGetUniformLocation(this->shadowmapShader, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
 	// LookAt camera (position, target/direction, up)
-	glm::vec3 cameraPos = glm::vec3(0, 3, 3);
-	glm::vec3 cameraFront = glm::vec3(0, -1, -1);
-	glm::mat4 view = glm::lookAt(glm::vec3(posCamX, posCamY, posCamZ), glm::vec3(viewCamX, viewCamY, viewCamZ), glm::vec3(upCamX, upCamY, upCamZ));
-	GLint viewLoc = glGetUniformLocation(this->shaderProgram, "view");
-	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+	glm::vec3 cameraPos = glm::vec3(0, 5, -20);
+	glm::vec3 cameraFront = glm::vec3(0, 0, 0);
+	glm::mat4 view = glm::lookAt(cameraPos, cameraFront, glm::vec3(0, 1, 0));
+	glUniformMatrix4fv(glGetUniformLocation(this->shadowmapShader, "view"), 1, GL_FALSE, glm::value_ptr(view));
 
-	DrawColoredLemari();
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-	DrawColoredLantai();
+	// Setting Light Attributes
+	glUniformMatrix4fv(glGetUniformLocation(this->shadowmapShader, "lightSpaceMatrix"), 1, GL_FALSE, glm::value_ptr(lightSpaceMatrix));
+	glUniform3f(glGetUniformLocation(this->shadowmapShader, "viewPos"), cameraPos.x, cameraPos.y, cameraPos.z);
+	glUniform3f(glGetUniformLocation(this->shadowmapShader, "lightPos"), -2.0f, 4.0f, -1.0f);
 
+	// Configure Shaders
+	glUniform1i(glGetUniformLocation(this->shadowmapShader, "diffuseTexture"), 0);
+	glUniform1i(glGetUniformLocation(this->shadowmapShader, "shadowMap"), 1);
+
+	// Render floor
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, plane_texture);
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, depthMap);
+	DrawTexturedPlane(this->shadowmapShader);
+
+	// Render cube
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, cube_texture);
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, depthMap);
+	DrawTexturedCube(this->shadowmapShader);
+
+
+	glDisable(GL_BLEND);
 	glDisable(GL_DEPTH_TEST);
 }
 
-void Demo::BuildColoredLemari() {
+void Demo::BuildTexturedCube() {
 	// load image into texture memory
 	// ------------------------------
 	// Load and create a texture 
-	glGenTextures(1, &texture);
-	glBindTexture(GL_TEXTURE_2D, texture);
+	glGenTextures(1, &cube_texture);
+	glBindTexture(GL_TEXTURE_2D, cube_texture);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	int width, height;
-	unsigned char* image = SOIL_load_image("lemari.png", &width, &height, 0, SOIL_LOAD_RGBA);
+	unsigned char* image = SOIL_load_image("lemaritransparan.png", &width, &height, 0, SOIL_LOAD_RGBA);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
 	SOIL_free_image_data(image);
 	glBindTexture(GL_TEXTURE_2D, 0);
+
+	
+
 
 	// set up vertex data (and buffer(s)) and configure vertex attributes
 	// ------------------------------------------------------------------
 	float vertices[] = {
 		// format position, tex coords
 		// front
-		-2.0, -0.5, 1.5, 0, 0,  // 0
-		2.0, -0.5, 1.5, 1, 0,   // 1
-		2.0,  6.0, 1.5, 1, 1,   // 2
-		-2.0,  6.0, 1.5, 0, 1,  // 3
-		
+		-2.0, -0.5, 1.5, 0, 0, 0.0f,  0.0f,  1.0f, // 0
+		2.0, -0.5, 1.5, 1, 0,  0.0f,  0.0f,  1.0f, // 1
+		2.0,  6.0, 1.5, 1, 1, 0.0f,  0.0f,  1.0f,  // 2
+		-2.0,  6.0, 1.5, 0, 1, 0.0f,  0.0f,  1.0f, // 3
+
 		//right
-		2.0,  6.0,  1.5, 0, 0,  // 4
-		2.0,  6.0, -1.5, 1, 0,  // 5
-		2.0, -0.5, -1.5, 1, 1,  // 6
-		2.0, -0.5,  1.5, 0, 1,  // 7
+		2.0,  6.0,  1.5, 0, 0, 1.0f,  0.0f,  0.0f, // 4
+		2.0,  6.0, -1.5, 1, 0,  1.0f,  0.0f,  0.0f,// 5
+		2.0, -0.5, -1.5, 1, 1,  1.0f,  0.0f,  0.0f,// 6
+		2.0, -0.5,  1.5, 0, 1,  1.0f,  0.0f,  0.0f,// 7
 
 		//back
-		-2.0, -0.5, -1.5, 0, 0, // 8 
-		2.0, -0.5, -1.5, 1, 0, // 9
-		2.0,  6.0, -1.5, 1, 1, // 10
-		-2.0,  6.0, -1.5, 0, 1, // 11
+		-2.0, -0.5, -1.5, 0, 0, 0.0f,  0.0f,  -1.0f,// 8 
+		2.0, -0.5, -1.5, 1, 0, 0.0f,  0.0f,  -1.0f,// 9
+		2.0,  6.0, -1.5, 1, 1, 0.0f,  0.0f,  -1.0f,// 10
+		-2.0,  6.0, -1.5, 0, 1, 0.0f,  0.0f,  -1.0f,// 11
 
 		//left
-		-2.0, -0.5, -1.5, 0, 0, // 12
-		-2.0, -0.5,  1.5, 1, 0, // 13
-		-2.0,  6.0,  1.5, 1, 1, // 14
-		-2.0,  6.0, -1.5, 0, 1, // 15
+		-2.0, -0.5, -1.5, 0, 0, -1.0f,  0.0f,  0.0f, // 12
+		-2.0, -0.5,  1.5, 1, 0, -1.0f,  0.0f,  0.0f,// 13
+		-2.0,  6.0,  1.5, 1, 1, -1.0f,  0.0f,  0.0f,// 14
+		-2.0,  6.0, -1.5, 0, 1, -1.0f,  0.0f,  0.0f,// 15
 
 		//upper
-		2.0, 6.0,  1.5, 0, 0,   // 16
-		-2.0, 6.0,  1.5, 1, 0,  // 17
-		-2.0, 6.0, -1.5, 1, 1,  // 18
-		2.0, 6.0, -1.5, 0, 1,   // 19
+		2.0, 6.0,  1.5, 0, 0,   0.0f,  1.0f,  0.0f, // 16
+		-2.0, 6.0,  1.5, 1, 0,  0.0f,  1.0f,  0.0f, // 17
+		-2.0, 6.0, -1.5, 1, 1,  0.0f,  1.0f,  0.0f, // 18
+		2.0, 6.0, -1.5, 0, 1,   0.0f,  1.0f,  0.0f, // 19
 
 		//bottom
-		-2.0, -0.5, -1.5, 0, 0, // 20
-		2.0, -0.5, -1.5, 1, 0,  // 21
-		2.0, -0.5,  1.5, 1, 1,  // 22
-		-2.0, -0.5,  1.5, 0, 1, // 23
+		-2.0, -0.5, -1.5, 0, 0, 0.0f,  -1.0f,  0.0f,// 20
+		2.0, -0.5, -1.5, 1, 0,  0.0f,  -1.0f,  0.0f,// 21
+		2.0, -0.5,  1.5, 1, 1, 0.0f,  -1.0f,  0.0f, // 22
+		-2.0, -0.5,  1.5, 0, 1, 0.0f,  -1.0f,  0.0f,// 23
 	};
 
 	unsigned int indices[] = {
@@ -208,25 +256,29 @@ void Demo::BuildColoredLemari() {
 		20, 22, 21, 20, 23, 22   // bottom
 	};
 
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
+	glGenVertexArrays(1, &cubeVAO);
+	glGenBuffers(1, &cubeVBO);
+	glGenBuffers(1, &cubeEBO);
 	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-	glBindVertexArray(VAO);
+	glBindVertexArray(cubeVAO);
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, cubeEBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	// define position pointer layout 0
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(0 * sizeof(GLfloat)));
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(0 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(0);
 
 	// define texcoord pointer layout 1
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(1);
+
+	// define normal pointer layout 2
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(5 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(2);
 
 	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -240,24 +292,32 @@ void Demo::BuildColoredLemari() {
 
 }
 
-void Demo::DrawColoredLemari()
+void Demo::DrawTexturedCube(GLuint shader)
 {
-	glUseProgram(shaderProgram);
+	UseShader(shader);
+	glBindVertexArray(cubeVAO);
 
 	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, texture);
-	glUniform1i(glGetUniformLocation(this->shaderProgram, "ourTexture"), 0);
+	glBindTexture(GL_TEXTURE_2D, cube_texture);
+	glUniform1i(glGetUniformLocation(this->shadowmapShader, "material.diffuse"), 0);
 
-	glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, stexture);
+	glUniform1i(glGetUniformLocation(this->shadowmapShader, "material.specular"), 1);
+
+	GLint shininessMatLoc = glGetUniformLocation(this->shadowmapShader, "material.shininess");
+	glUniform1f(shininessMatLoc, 0.4f);
+
+	glBindVertexArray(cubeVAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
 
 	glm::mat4 model;
-	model = glm::translate(model, glm::vec3(0, 1, 0));
+	model = glm::translate(model, glm::vec3(0, 0.5f, 0));
 
-	model = glm::rotate(model, angle, glm::vec3(0, -1, 0));
+	model = glm::rotate(model, angle, glm::vec3(0, 1, 0));
 
 	model = glm::scale(model, glm::vec3(1, 1, 1));
 
-	GLint modelLoc = glGetUniformLocation(this->shaderProgram, "model");
+	GLint modelLoc = glGetUniformLocation(shader, "model");
 	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
 	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
@@ -266,79 +326,111 @@ void Demo::DrawColoredLemari()
 	glBindVertexArray(0);
 }
 
-void Demo::BuildColoredLantai()
+void Demo::BuildTexturedPlane()
 {
 	// Load and create a texture 
-	glGenTextures(1, &texture2);
-	glBindTexture(GL_TEXTURE_2D, texture2);
+	glGenTextures(1, &plane_texture);
+	glBindTexture(GL_TEXTURE_2D, plane_texture);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	int width, height;
-	unsigned char* image = SOIL_load_image("lantai.png", &width, &height, 0, SOIL_LOAD_RGBA);
+	unsigned char* image = SOIL_load_image("wood.png", &width, &height, 0, SOIL_LOAD_RGBA);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
 	glGenerateMipmap(GL_TEXTURE_2D);
 	SOIL_free_image_data(image);
 	glBindTexture(GL_TEXTURE_2D, 0);
 
+
+
 	// Build geometry
 	GLfloat vertices[] = {
 		// format position, tex coords
 		// bottom
-		-50.0, -0.5, -50.0,  0,  0,
-		 50.0, -0.5, -50.0, 50,  0,
-		 50.0, -0.5,  50.0, 50, 50,
-		-50.0, -0.5,  50.0,  0, 50,
+		-50.0, -0.5, -50.0,  0,  0, 0.0f,  1.0f,  0.0f,
+		50.0, -0.5, -50.0, 50,  0, 0.0f,  1.0f,  0.0f,
+		50.0, -0.5,  50.0, 50, 50, 0.0f,  1.0f,  0.0f,
+		-50.0, -0.5,  50.0,  0, 50, 0.0f,  1.0f,  0.0f,
 
 
 	};
 
 	GLuint indices[] = { 0,  2,  1,  0,  3,  2 };
 
-	glGenVertexArrays(1, &VAO2);
-	glGenBuffers(1, &VBO2);
-	glGenBuffers(1, &EBO2);
+	glGenVertexArrays(1, &planeVAO);
+	glGenBuffers(1, &planeVBO);
+	glGenBuffers(1, &planeEBO);
 
-	glBindVertexArray(VAO2);
+	glBindVertexArray(planeVAO);
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+	glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO2);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, planeEBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	// Position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), 0);
 	glEnableVertexAttribArray(0);
 	// TexCoord attribute
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(1);
+	// Normal attribute
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(5 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(2);
 
 	glBindVertexArray(0); // Unbind VAO
 }
 
 
 
-void Demo::DrawColoredLantai()
+void Demo::DrawTexturedPlane(GLuint shader)
 {
-	glUseProgram(shaderProgram);
 
-	glActiveTexture(GL_TEXTURE1);
-	glBindTexture(GL_TEXTURE_2D, texture2);
-	glUniform1i(glGetUniformLocation(this->shaderProgram, "ourTexture"), 1);
-
-	glBindVertexArray(VAO2); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+	UseShader(shader);
+	glBindVertexArray(planeVAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
 
 	glm::mat4 model;
-	GLint modelLoc = glGetUniformLocation(this->shaderProgram, "model");
+	GLint modelLoc = glGetUniformLocation(shader, "model");
 	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-	
+
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 	glBindTexture(GL_TEXTURE_2D, 0);
 	glBindVertexArray(0);
+}
+
+void Demo::BuildDepthMap() {
+	// configure depth map FBO
+	// -----------------------
+	glGenFramebuffers(1, &depthMapFBO);
+	// create depth texture
+	glGenTextures(1, &depthMap);
+	glBindTexture(GL_TEXTURE_2D, depthMap);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, this->SHADOW_WIDTH, this->SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+	float borderColor[] = { 1.0, 1.0, 1.0, 1.0 };
+	glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+
+	// attach depth texture as FBO's depth buffer
+	glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
+	glDrawBuffer(GL_NONE);
+	glReadBuffer(GL_NONE);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+void Demo::BuildShaders()
+{
+	// build and compile our shader program
+	// ------------------------------------
+	shadowmapShader = BuildShader("shadowMapping.vert", "shadowMapping.frag", nullptr);
+	depthmapShader = BuildShader("depthMap.vert", "depthMap.frag", nullptr);
 }
 
 void Demo::InitCamera()
@@ -393,6 +485,6 @@ void Demo::RotateCamera(float speed)
 
 
 int main(int argc, char** argv) {
-	RenderEngine &app = Demo();
+	RenderEngine& app = Demo();
 	app.Start("Camera: Free Camera Implementation", 800, 600, false, false);
 }
--- Demo.h
+++ Demo.h
@@ -13,21 +13,23 @@ public:
 	Demo();
 	~Demo();
 private:
+	GLuint depthmapShader, shadowmapShader, cubeVBO, cubeVAO, cubeEBO, cube_texture, planeVBO, planeVAO, planeEBO, plane_texture, stexture, stexture2, depthMapFBO, depthMap;
 	GLuint shaderProgram, VBO, VAO, EBO, texture, VBO2, VAO2, EBO2, texture2;
-	float viewCamX, viewCamY, viewCamZ, upCamX, upCamY, upCamZ, posCamX, posCamY, posCamZ, CAMERA_SPEED, fovy;
-	float angle = 0;
+	const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
+	float angle = 0, viewCamX, viewCamY, viewCamZ, upCamX, upCamY, upCamZ, posCamX, posCamY, posCamZ, CAMERA_SPEED, fovy;
 	virtual void Init();
 	virtual void DeInit();
 	virtual void Update(double deltaTime);
 	virtual void Render();
-	virtual void ProcessInput(GLFWwindow *window);
-	void BuildColoredLemari();
-	void BuildColoredLantai();
-	void DrawColoredLemari();
-	void DrawColoredLantai();
+	virtual void ProcessInput(GLFWwindow* window);
+	void BuildTexturedCube();
+	void BuildTexturedPlane();
+	void DrawTexturedCube(GLuint shader);
+	void DrawTexturedPlane(GLuint shader);
+	void BuildDepthMap();
+	void BuildShaders();
 	void MoveCamera(float speed);
 	void StrafeCamera(float speed);
 	void RotateCamera(float speed);
 	void InitCamera();
 };
-
